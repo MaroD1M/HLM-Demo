e391aab619ee7b59bdcabdcf502ae59713f7e74c
MaroD1M <134044063+MaroD1M@users.noreply.github.com>
2026-05-09 00:18:31 +0800

    回退代码与样式到v0.2.5基线

--- core/routes/web.py
+++ core/routes/web.py
@@ -718,7 +718,6 @@ def init_web_routes(ctx: RouteDeps):
             mapping_q = mapping_q.filter(
                 (FileLinkMap.source_path.like(like)) |
                 (FileLinkMap.dest_path.like(like)) |
-                (FileLinkMap.file_key.like(like)) |
                 (FileLinkMap.torrent_hash.like(like))
             )
         if hash_state == 'linked':
